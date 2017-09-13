368cd584216498926120197992a027d8451c1aae
Andrew Van Slaars <avanslaars@gmail.com>
2017-09-13 17:30:00 -0400

    Added more Maybe material

--- adts/Maybe.js
+++ adts/Maybe.js
@@ -11,14 +11,14 @@ const { Just, Nothing } = Maybe
 
 // const maybeThree = Maybe.of(3)
 const maybeThree = new Maybe(3)
-console.log(maybeThree) // Just 3
+console.log(maybeThree.inspect()) // Just 3
 
 const justThree = Just(3)
-console.log(justThree) // Just 3
+console.log(justThree.inspect()) // Just 3
 
 // Nothing doesn't carry a value - passing this an arg will still yield only Nothing
 const aNothing = Nothing()
-console.log(aNothing) // Nothing
+console.log(aNothing.inspect()) // Nothing
 
 /**
  * You might think null or undefined would automatically be converted to a Nothing
@@ -26,15 +26,17 @@ console.log(aNothing) // Nothing
  */
 // const justNull = Maybe.of(null)
 const justNull = new Maybe(null)
-console.log(justNull) // Just null
+console.log(justNull.inspect()) // Just null
 
 // We can get a nothing using the `zero` method
 const zNothing = Maybe.zero()
-console.log(zNothing) // Nothing
+console.log(zNothing.inspect()) // Nothing
 
 /**
  * To get back a Just or Nothing we can create a function and make the decision
  * ourselves
+ * None of these are being logged, but throw any of the resulting values in a console.log
+ * to verify the output supplied in the comment
  */
 
 // This will give is a Nothing for any falsy value
@@ -83,10 +85,10 @@ const betweenOneAndTen = n => n >= 1 && n <= 10
 const maybeBetweenOneAndTen = safe(betweenOneAndTen)
 
 const shouldBeJust = maybeBetweenOneAndTen(4)
-console.log(shouldBeJust) // Just 4
+console.log(shouldBeJust.inspect()) // Just 4
 
 const shouldBeNothing = maybeBetweenOneAndTen(12)
-console.log(shouldBeNothing) // Nothing
+console.log(shouldBeNothing.inspect()) // Nothing
 
 /**
  * Now that we can create a maybe, let's head over to the MaybeMethods.js file
--- adts/MaybeMethods.js
+++ adts/MaybeMethods.js
@@ -1,8 +1,12 @@
-// const crocks = require('crocks')
+const crocks = require('crocks')
 
-// alt, ap, chain, coalesce, concat, equals, either, map, option, sequence, traverse
+// ap, coalesce, concat, equals, sequence, traverse
 
-// const { Maybe } = crocks
+const { Maybe, safe, prop } = crocks
+
+// Some simple utility functions for demo purposes
+const inc = n => n + 1
+const dbl = n => n * 2
 
 /**
  * Now that we can make a maybe, let's see what we can do with them...
@@ -12,5 +16,125 @@
  * Maybe comes in...
  */
 
-// const theJust = Maybe.Just(5)
-// const theNothing = Maybe.Nothing()
+const theJust = Maybe.Just(5)
+const theNothing = Maybe.Nothing()
+
+/**
+ * We'll use `map` to access our data and transform it
+ * `map` unwraps the value from the container, applies the supplied function
+ * and returns the same type, containing the new value
+ */
+
+const incDblMapped = theJust.map(inc).map(dbl)
+console.log(incDblMapped.inspect()) // Just 12
+
+// Now let's run the same maps on the Maybe that we know is a Nothing...
+const incDblMapped2 = theNothing.map(inc).map(dbl)
+console.log(incDblMapped2.inspect()) // Nothing
+
+/**
+ * Look at that, we just get a Nothing back. No attempt was made to execute our functions...
+ * when `map` is called on a Nothing, a Nothing is returned and the function is not executed
+ */
+
+/**
+ * The `safetyFirst` function is a result of using Boolean as the predicate for
+ * the `safe` function, so it takes a value and returns a Maybe 
+ * (Nothing for a falsy value or Just <value>)
+ * Let's see what happens when we `map` over our Just with this function
+ */
+
+const safetyFirst = safe(Boolean)
+const reallySafe = theJust.map(safetyFirst)
+console.log(reallySafe.inspect()) // Just Just 5
+
+/**
+ * That isn't a typo - the return value here is `Just Just 5`. We have a Maybe wrapped in another Maybe
+ * This means we have and extra layer between us and our value. If we wanted to execute another function
+ * in a second `map` - we'd have to account for getting a Maybe as the argument for that function
+ * and then if we still wanted to transform the nested value, we'd have to access it within the nested Maybe
+ * using `map` or some other method that would unwrap the value...
+ * 
+ * So, this is where we should reach for `chain` - sometimes referred to as `flatMap` in other libraries,
+ * `chain` will "flatten" our nested Maybe structure, leaving us with our value in a single Just
+ * 
+ * I'd like to note that this example was mostly about showing what happens when you `map` where you should `chain`
+ * The code below on its own is pointless, but demonstrates the difference between `map` and `chain`
+ */
+
+const normalSafe = theJust.chain(safetyFirst)
+console.log(normalSafe.inspect()) // Just 5
+
+/**
+ * Let's look at ways to extract our value from a Maybe...
+ * We'll work with our 2 known Maybe values so results are
+ * completely predictable
+ */
+
+const withValue = theJust.map(inc).map(dbl) // Just 12
+const noValue = theNothing.map(inc).map(dbl) // Nothing
+
+// option - returns the unwrapped value or the supplied default for a Nothing
+const valueFromOption = withValue.option(-3)
+console.log(valueFromOption) // 12
+
+const nothingFromOption = noValue.option(-3)
+console.log(nothingFromOption) // -3
+
+// either - runs a "left" function for a Nothing or a "right" function for a Just
+const valueFromEither = withValue.either(
+  () => 'No Value to Transform',
+  n => `__${n}__`
+)
+console.log(valueFromEither) // __12__
+
+const nothingFromEither = noValue.either(
+  () => 'No Value to Transform',
+  n => `__${n}__`
+)
+console.log(nothingFromEither) // "No Value to Transform"
+
+/**
+ * The Maybe keeps our operations safe by skipping operations that might otherwise 
+ * throw an exception with a null or undefined value
+ * That's great, but sometimes you may want to run some operations anyway, just
+ * using a default value if you end up with a Nothing
+ * alt allows you to define a default Maybe that is returned in the event of a Nothing
+ * 
+ */
+
+const altWithValue = withValue.alt(Maybe.Just(10))
+console.log(altWithValue.inspect()) // Just 12
+
+const altWithNothing = noValue.alt(Maybe.Just(10))
+console.log(altWithNothing.inspect()) // Just 10 - returning the default Maybe
+
+/**
+ * Let's look at an example of using alt to carry on with a default value
+ * Here we have an input value that is missing the name property and a default
+ * which has name defined.
+ * We'll use the `prop` function supplied by crocks. It'll get the value of a property
+ * but it will do it in a safe manner by returning a Maybe. If the value can't be found,
+ * we'll get back a Nothing
+ */
+
+const defaultName = 'default'
+const inputUser1 = { age: 30 } // missing the name prop
+const inputUser2 = { name: 'charlotte', age: 1 }
+
+const theName1 = prop('name', inputUser1)
+const theName2 = prop('name', inputUser2)
+console.log(theName1.inspect()) // Nothing
+console.log(theName2.inspect()) // Just "charlotte"
+
+const cappedName1 = theName1
+  .alt(Maybe.of(defaultName)) // theName1 is a Nothing, so we'll end up with Just "default" here
+  .map(str => str.toUpperCase())
+
+console.log(cappedName1.inspect()) // Just "DEFAULT"
+
+const cappedName2 = theName2
+  .alt(Maybe.of(defaultName)) // theName is a Nothing, so we'll end up with Just "default" here
+  .map(str => str.toUpperCase())
+
+console.log(cappedName2.inspect()) // Just "CHARLOTTE"
