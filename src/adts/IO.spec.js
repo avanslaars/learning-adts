3422f8354e66a9da96b62dfafbffafcc5b233875
Andrew Van Slaars <avanslaars@gmail.com>
2017-12-04 21:51:52 -0500

    Basic IO examples

---
+++ src/adts/IO.spec.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect
+
+describe('IO', () => {
+  const IO = require('crocks/IO')
+  context('construction', () => {
+    /**
+     * The goal here is to wrap impure/unsafe code
+     * So something like the safeDOM could be wrapped
+     * in an IO?
+     */
+    it('Can be created with a function in new', () => {
+      const io = new IO(function () {
+        return 10
+      })
+      expect(io.inspect()).to.equal('IO Function')
+
+      const result = io.map(n => n * 2)
+      expect(result.inspect()).to.eql('IO Function')
+      const out = result.run()
+      expect(out).to.equal(20)
+    })
+
+    it('Can be created with a value in of', () => {
+      const io = IO.of(10)
+      expect(io.inspect()).to.equal('IO Function')
+
+      const result = io.map(n => n * 2)
+      expect(result.inspect()).to.eql('IO Function')
+      const out = result.run()
+      expect(out).to.equal(20)
+    })
+  })
+})
